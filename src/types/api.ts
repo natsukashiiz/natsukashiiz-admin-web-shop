fcf41cb9082b14069ae612ed3e825ea617437c7b
ɴᴀᴛsᴜᴋᴀsʜɪɪᴢ <74323401+natsukashiiz@users.noreply.github.com>
2024-07-08 22:22:13 +0700

    feat(types): add api type

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,8 +1,9 @@
 import type { AxiosResponse } from 'axios'
+import type { AdminRoles, DiscountType } from './enum'
 
 export type ApiResponse<T> = Promise<AxiosResponse<T>>
 
-export type PageResponse<T> = {
+export interface PageResponse<T> {
   list: T[]
   total: number
 }
@@ -34,4 +35,93 @@ export interface LoginRequest {
 
 export interface RefreshTokenRequest {
   refreshToken: string
+}
+
+export interface ManagerResponse {
+  id: number
+  createdAt: Date
+  updatedAt: Date
+  username: string
+  role: AdminRoles
+}
+
+export interface CustomerResponse {
+  id: number
+  createdAt: Date
+  updatedAt: Date
+  email: string
+  username: string
+  avatar: string | null
+  verified: boolean
+  deleted: boolean
+  deletedAt: string | null
+}
+export interface ProductResponse {
+  id: number
+  createdAt: Date
+  updatedAt: Date
+  name: string
+  category: CategoryResponse
+  options: ProductOptionResponse[]
+  images: ImageResponse[]
+  description: string
+  views: number
+  orders: number
+  rating: number
+  reviews: number
+}
+
+export interface CategoryResponse {
+  id: number
+  createdAt: Date
+  updatedAt: Date
+  name: string
+  thumbnail: string
+  sort: number
+}
+
+export interface ImageResponse {
+  id: number
+  createdAt: Date
+  updatedAt: Date
+  url: string
+  sort: number
+}
+
+export interface ProductOptionResponse {
+  id: number
+  createdAt: Date
+  updatedAt: Date
+  name: string
+  price: number
+  quantity: number
+  sort: number
+  image: ImageResponse
+}
+
+export interface CarouselResponse {
+  id: number
+  createdAt: Date
+  updatedAt: Date
+  title: string
+  imageUrl: string
+  sort: number
+}
+
+export interface VoucherResponse {
+  id: number
+  createdAt: Date
+  updatedAt: Date
+  code: string
+  discount: number
+  discountType: DiscountType
+  maxDiscount: number | null
+  minOrderPrice: number
+  quantity: number
+  product: ProductResponse | null
+  category: CategoryResponse | null
+  beginAt: Date
+  expiredAt: Date
+  status: string
+  thumbnail: string
 }
