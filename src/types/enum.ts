265d3839a23d2b31d92757ad1d81b99ae0bf256c
ɴᴀᴛsᴜᴋᴀsʜɪɪᴢ <74323401+natsukashiiz@users.noreply.github.com>
2024-07-08 21:12:08 +0700

    feat(enum): add AdminRoles, VoucherStatus

--- src/types/enum.ts
+++ src/types/enum.ts
@@ -16,3 +16,13 @@ export enum SocialProviders {
   FACEBOOK = 'FACEBOOK',
   GOOGLE = 'GOOGLE'
 }
+
+export enum AdminRoles {
+  ADMIN = 'ADMIN',
+  SUPER_ADMIN = 'SUPER_ADMIN'
+}
+
+export enum VoucherStatus {
+  ACTIVE = 'ACTIVE',
+  INACTIVE = 'INACTIVE'
+}
